3039a27e4dba9311ee7eac94d9a1c252ada30af9
jnozsc <jnozsc@gmail.com>
2013-04-30 13:07:01 -0400

    add header and footer

---
+++ application/views/footer.php
@@ -0,0 +1,10 @@
+	</div>
+	<div id="push"></div>
+	    </div>
+	<div id="footer">
+	      <div class="container">
+	        <p class="muted credit">Copyleft by <a href="#">Hongquan Xie</a> and <a href="#">Shuchang Zhong</a>.</p>
+	      </div>
+	    </div>
+  </body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>JINGO</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+	<div id="wrap">
+	
+	<div class="navbar navbar-inverse navbar-fixed-top">
+	      <div class="navbar-inner">
+	        <div class="container">
+	          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="brand" href="#">Project name</a>
+	          <div class="nav-collapse collapse">
+	            <ul class="nav">
+	              <li class="active"><a href="#">Home</a></li>
+	              <li><a href="#about">About</a></li>
+	              <li><a href="#about2">About2</a></li>			
+	            </ul>
+				<ul class="nav pull-right">
+					<?php if ture ?>  <!-- TODO if not login -->
+					<li><a href="#login" class ="pull-right">JOIN US!</a><li>
+					<?php else ?> <!-- TODO if login -->
+				   <li><a href="#logout" class ="pull-right">LOGOUT:(</a><li>
+					<?php endif ?>
+				</ul>
+	          </div><!--/.nav-collapse -->
+	        </div>
+	      </div>
+	    </div>
+		
+	<div class="container"> 
+		<b> It works! </b>
+		<!-- insert our page here -->
+	
+	
