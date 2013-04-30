be6b64da712f3fc1860d5e83eb22398e36bbdd22
xie <izzy.xie@gmail.com>
2013-04-30 19:10:25 -0400

    set default controller as pub

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "pub";
 $route['404_override'] = '';
 $route['logout'] = "pub/logout";
 $route['login'] = "pub/login";
