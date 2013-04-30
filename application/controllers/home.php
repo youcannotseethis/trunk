7d5bc63a4129877d3e0b635ba1c89bb06875cd0e
xie <izzy.xie@gmail.com>
2013-04-30 10:10:47 -0400

    construct of Home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -6,6 +6,29 @@ class Home extends CI_Controller {
 		#$this->load->view('4sq.html');
 	}
 	
+ 	public function __construct(){
+		parent::__construct();
+		#session_start();
+		#$this->user = $_SESSION['user'];
+		#$this->user_id = $this->user['id'];
+		$this->data = array();
+		$this->title = '';
+    }
+	public function __destruct(){
+		$data = $this->data;
+		$data['title'] = $this->title;
+		#if(!(isset($this->no_view)&&$this->no_view))
+		#	echo make_view($this,$data);
+	}	
+	public function user(){
+		#$user_id = $this->input->post('user_id');
+		
+		$this->load->model('User');
+		$this->User->id = '2';
+		$user = current($this->User->get());
+		$this->
+		dump($user);
+	}
 	public function foursquare(){
 		#echo 'm';
 		$this->load->view('4sq.html');
