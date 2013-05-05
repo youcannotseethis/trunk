05b3e0c68cd600ae7d48819dbc3393da046ded58
jnozsc <jnozsc@gmail.com>
2013-05-05 19:09:43 -0400

    4sq

--- application/views/4sq.php
+++ application/views/4sq.php
@@ -1,17 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <head>
-    
-  
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js" type="text/javascript" id="jquery"></script>
-  
-  
-
     <link href="https://ss1.4sqi.net/styles/third_party/leaflet-811bbe17aab6403fa760a25378f6e091.css" type="text/css" rel="stylesheet" />
     <link href="https://ss1.4sqi.net/styles/third_party/apisamples-0392cfe95c9fd32087ab6d80fcb80175.css" type="text/css" rel="stylesheet" />
-
     <script src="https://ss1.4sqi.net/scripts/apisamples-35608dc9c26343e74f5d99fc20bae6c5.js" type="text/javascript"></script>
     <script src="https://ss0.4sqi.net/scripts/third_party/jquery.ba-bbq-eddd4adf74d0c1310a401475178c57df.js" type="text/javascript"></script>
     <script src="https://ss1.4sqi.net/scripts/third_party/leaflet-33a0ffb9d515d9e8e130778090a06816.js" type="text/javascript"></script>
@@ -21,7 +14,6 @@
     <script type="text/javascript">
     //<![CDATA[
 
-
       /**
        * Sample application that uses the foursquare API, Leaflet, and the MapBox API
        * to show trending locations.
@@ -29,7 +21,7 @@
       function Explore(apiKey, authUrl, apiUrl, mapboxId) {
         this.foursquare = new Foursquare(apiKey, authUrl, apiUrl);
         this.map = new L.Map('map')
-          .setView(new L.LatLng(40.7, -74), 14);
+          .setView(new L.LatLng(40.729515,-73.997188), 14);
         var map = this.map;
         var https = (location.protocol === 'https:'),
                     base_https = 'https://dnv9my2eseobd.cloudfront.net/v3/',
@@ -39,8 +31,17 @@
           map.addLayer(new wax.leaf.connector(tilejson));
         });
         this.map.on('move', bind(this.onCenterChange, this));
-      }
+		var popup = L.popup();
 
+		function onMapClick(e) {
+		    popup
+		        .setLatLng(e.latlng)
+		        .setContent("You clicked the map at " + e.latlng.toString())
+		        .openOn(map);
+		}
+		map.on('click', onMapClick);
+      }
+	  
       /**
        * Trigger first data fetch.
        */
@@ -98,8 +99,6 @@
         
       })
     </script>
-  
-	
 </head>
 <body>
     <div id="map" style="width: 550px; height: 550px; position: absolute;"></div>
@@ -107,17 +106,6 @@
       <h1>What's hot right now?</h1>
       Hover over the markers in an area to see what's trending.
     </div>
-    <div class="note">
-      This is a <a href="../samples">sample foursquare API application</a>. Please view the source and remix it in your own applications!
-    </div>
-  <script src="/ajax_request/liftAjax.js" type="text/javascript"></script>
-
-	
-<script type="text/javascript">
-// <![CDATA[
-
-var lift_page = "F1119963489623J0IQCU";
-// ]]>
-</script></body>
+</body>
 </html>  
   
